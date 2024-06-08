a61a59b5494abcb25342bdf64720598f56eb813e
Sommya Jain <sommyajain2005@gmail.com>
2024-06-09 00:50:10 +0530

    first question

--- Assignment 1.py
+++ Assignment 1.py
@@ -1,1 +1,5 @@
-print("Hello, world!")
+#Question 1 - Write a program that takes two numbers as input and prints their sum. 
+a=int(input("Enter first number :"))
+b=int(input("Enter second number :"))
+sum=a+b
+print("Sum of the two numbers :",sum)
