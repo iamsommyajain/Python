fdaf081940adb2f7cae6841648ca612ffc4fdba7
Sommya Jain <sommyajain2005@gmail.com>
2024-06-09 10:01:55 +0530

    fifth commit

--- Assignment 1.py
+++ Assignment 1.py
@@ -1,4 +1,3 @@
-'''
 #Question 1 - Write a program that takes two numbers as input and prints their sum. 
 a=int(input("Enter first number :"))
 b=int(input("Enter second number :"))
@@ -22,8 +21,33 @@ print("The factorial of the given number :",fact)
 #Question 4 - Write a program that asks the user for their name and then prints a greeting message. 
 name=input("Enter your name :")
 print("Hello {}, Welcome to our session".format(name))
-'''
 
 #Question 5 - Write a program that takes a string input from the user and writes it to a text file. 
-str=input("Enter text :")
-print(str,file=r"C:\Users\HP\OneDrive\Desktop\Python\textfile.txt")
+samplefile=open("textfile.txt","w")
+str=input("Enter your string :")
+print(str,file=samplefile)
+
+#Question 6 - Write a program that reads the content of a text file and prints it to the console. 
+samplefile=open("textfile.txt","r")
+print(samplefile.read())
+
+#Question 7 - Write a python program that takes a string as input and returns its length. 
+str=input("Enter your string :")
+print("Length of given string is :",len(str))
+
+#Question 8 - Write a python program that concatenates two strings and returns the result.
+str1=input("Enter first string :")
+str2=input("Enter second string :")
+print("Concatenated string is :",str1+str2)
+
+#Question 9 - Write a python program that checks if a substring is present in a given string. 
+str=input("Enter string :")
+substr=input("Enter substring :")
+print(substr in str)
+
+#Question 10 - Write a python program that converts a given string to uppercase. 
+str=input("Enter string :")
+print(str.upper())
+
+#Question 11 - Write a python program that generates the first n numbers in the Fibonacci sequence. 
+
