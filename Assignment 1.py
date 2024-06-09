2e4fb5ae92c3aa4ec245cee42b5133082fb6de0a
Sommya Jain <sommyajain2005@gmail.com>
2024-06-09 11:16:19 +0530

    fifth commit

--- Assignment 1.py
+++ Assignment 1.py
@@ -21,3 +21,32 @@ print("The factorial of the given number :",fact)
 #Question 4 - Write a program that asks the user for their name and then prints a greeting message. 
 name=input("Enter your name :")
 print("Hello {}, Welcome to our session".format(name))
+
+#Question 5 - Write a program that takes a string input from the user and writes it to a text file. 
+samplefile=open("textfile.txt","w")
+str=input("Enter your string :")
+print(str,file=samplefile)
+
+#Question 6 - Write a program that reads the content of a text file and prints it to the console. 
+samplefile=open("textfile.txt","r")
+print(samplefile.read())
+
+#Question 7 - Write a python program that takes a string as input and returns its length. 
+str=input("Enter your string :")
+print("Length of given string is :",len(str))
+
+#Question 8 - Write a python program that concatenates two strings and returns the result.
+str1=input("Enter first string :")
+str2=input("Enter second string :")
+print("Concatenated string is :",str1+str2)
+
+#Question 9 - Write a python program that checks if a substring is present in a given string. 
+str=input("Enter string :")
+substr=input("Enter substring :")
+print(substr in str)
+
+#Question 10 - Write a python program that converts a given string to uppercase. 
+str=input("Enter string :")
+print(str.upper())
+
+#Question 11 - Write a python program that generates the first n numbers in the Fibonacci sequence.
